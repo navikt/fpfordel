2130d85b482899c5a717adc694a41f4bfe2645cf
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-15 14:42:12 +0100

    demonstrer feilen

--- web/src/main/java/no/nav/foreldrepenger/fordel/web/app/selftest/checks/InfotrygdSakRestHealthCheck.java
+++ web/src/main/java/no/nav/foreldrepenger/fordel/web/app/selftest/checks/InfotrygdSakRestHealthCheck.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.fordel.web.app.selftest.checks;
 
 import java.net.URI;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -10,7 +11,7 @@ import org.slf4j.LoggerFactory;
 import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 import no.nav.vedtak.konfig.KonfigVerdi;
 
-//@ApplicationScoped
+@ApplicationScoped
 public class InfotrygdSakRestHealthCheck extends WebServiceHealthCheck {
     private static final String DEFAULT_URI = "http://infotrygd-svangerskapspenger.default/actuator/health";
     private static final Logger LOG = LoggerFactory.getLogger(InfotrygdSakRestHealthCheck.class);
