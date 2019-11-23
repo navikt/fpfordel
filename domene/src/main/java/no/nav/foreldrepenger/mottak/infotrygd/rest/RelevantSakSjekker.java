b3990fa920396904dd9f4c343e268a5d8bcda45b
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-23 11:11:40 +0100

    imports

--- domene/src/main/java/no/nav/foreldrepenger/mottak/infotrygd/rest/RelevantSakSjekker.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/infotrygd/rest/RelevantSakSjekker.java
@@ -11,6 +11,7 @@ import java.time.Period;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.function.Predicate;
 
 import javax.enterprise.context.ApplicationScoped;
