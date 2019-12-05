e7ed11ff25e2c296e5b6669cda13f150d763859d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-12-05 09:57:04 +0100

    fjernet ubrukt logback konfig

--- run-java.sh
+++ run-java.sh
@@ -7,6 +7,5 @@ export JAVA_OPTS="${JAVA_OPTS:-} -Djava.security.egd=file:/dev/./urandom"
 
 export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.fordel.web.server.jetty.JettyServer"}
 export CLASSPATH="app.jar:lib/*"
-export LOGBACK_CONFIG=${LOGBACK_CONFIG:-"logback.xml"}
 
-exec java -cp ${CLASSPATH:-"app.jar:lib/*"} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS} -Dlogback.configurationFile=${LOGBACK_CONFIG?} -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
+exec java -cp ${CLASSPATH:-"app.jar:lib/*"} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS}  -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
