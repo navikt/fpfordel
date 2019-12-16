4f0b105fbafdc6818aeaf514c458abadb27ffef6
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-12-16 19:08:00 +0100

    Delete s3upload-init.sh

--- s3upload-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-JAVA_OPTS="${JAVA_OPTS} -XX:ErrorFile="./hs_error.log" -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heapdump.hprof "
-JAVA_OPTS="${JAVA_OPTS} -XX:OnOutOfMemoryError=/s3upload.sh -XX:OnError=/s3upload.sh "
-JAVA_OPTS="${JAVA_OPTS} -Xlog:gc:./gc.log "
-
-
-export JAVA_OPTS
