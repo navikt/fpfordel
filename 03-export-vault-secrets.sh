2b250ef4694b416e217f0421bc5db01a2bf97480
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-12-17 17:15:32 +0100

    ikke log pw

--- 03-export-vault-secrets.sh
+++ 03-export-vault-secrets.sh
@@ -2,7 +2,7 @@
 
 if test -f /var/run/secrets/nais.io/defaultDS/username;
 then
-    export  DEFAULTDS_USERNAME=$(cat /var/run/secrets/nais.io/defaultDS/username)
+   export  DEFAULTDS_USERNAME=$(cat /var/run/secrets/nais.io/defaultDS/username)
    echo "Setting DEFAULTDS_USERNAME to $DEFAULTDS_USERNAME"
     
 fi
@@ -10,6 +10,6 @@ fi
 if test -f /var/run/secrets/nais.io/defaultDS/password;
 then
     export  DEFAULTDS_PASSWORD=$(cat /var/run/secrets/nais.io/defaultDS/password)
-    echo "Setting DEFAULTDS_PASSWORD to $DEFAULTDS_PASSWORD"
+    echo "Setting DEFAULTDS_PASSWORD"
 fi
 echo "INIT SCRIPT DONE"
