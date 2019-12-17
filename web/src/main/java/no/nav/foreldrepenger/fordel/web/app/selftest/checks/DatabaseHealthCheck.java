81206b40d55e1b4b6d4affa9987f1dcaf4854caa
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2019-12-17 12:00:03 +0100

    FIX helsesjekk skal ikke se på kodeverdi. sjekket andre queries

--- web/src/main/java/no/nav/foreldrepenger/fordel/web/app/selftest/checks/DatabaseHealthCheck.java
+++ web/src/main/java/no/nav/foreldrepenger/fordel/web/app/selftest/checks/DatabaseHealthCheck.java
@@ -18,7 +18,7 @@ public class DatabaseHealthCheck extends ExtHealthCheck {
 
     private String jndiName;
 
-    private static final String SQL_QUERY = "select count(1) from KODEVERK";
+    private static final String SQL_QUERY = "select count(1) from PROSESS_TASK_TYPE";
     // må være rask, og bruke et stabilt tabell-navn
 
     private String endpoint = null; // ukjent frem til første gangs test
