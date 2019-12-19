a49eee546ba4ead38d81c492965c452453fc617f
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-12-19 10:00:31 +0100

    url fra vault

--- 03-export-vault-secrets.sh
+++ 03-export-vault-secrets.sh
@@ -12,4 +12,10 @@ then
     export  DEFAULTDS_PASSWORD=$(cat /var/run/secrets/nais.io/defaultDS/password)
     echo "Setting DEFAULTDS_PASSWORD"
 fi
+
+if test -f /var/run/secrets/nais.io/defaultDSconfig/jdbc_url;
+then
+    export  DEFAULTDS_URL=$(cat /var/run/secrets/nais.io/defaultDSconfig/jdbc_url)
+    echo "Setting DEFAULTDS_URL til $DEFAULTDS_URL"
+fi
 echo "INIT SCRIPT DONE"
