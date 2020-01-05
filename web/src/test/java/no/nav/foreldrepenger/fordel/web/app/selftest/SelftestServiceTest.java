abe4a631a4a91ed6584290877674b4901796e14e
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-05 13:48:14 +0100

    more tags

--- web/src/test/java/no/nav/foreldrepenger/fordel/web/app/selftest/SelftestServiceTest.java
+++ web/src/test/java/no/nav/foreldrepenger/fordel/web/app/selftest/SelftestServiceTest.java
@@ -10,22 +10,20 @@ import java.time.LocalDateTime;
 
 import javax.ws.rs.core.Response;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.codahale.metrics.health.HealthCheck;
 
-@SuppressWarnings("resource")
 public class SelftestServiceTest {
 
-    private SelftestService service; // objektet vi tester
-
+    private SelftestService service;
     private Selftests mockSelftests;
 
     private static final String MSG_KRITISK_FEIL = "kritisk feil";
     private static final String MSG_IKKEKRITISK_FEIL = "ikke-kritisk feil";
 
-    @Before
+    @BeforeEach
     public void setup() {
         mockSelftests = mock(Selftests.class);
         service = new SelftestService(mockSelftests);
@@ -35,9 +33,7 @@ public class SelftestServiceTest {
     public void test_doGet_alleDeltesterOk() {
         SelftestResultat resultat = lagSelftestResultat(true, true);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(APPLICATION_JSON, false);
-
         assertThat(response).isNotNull();
     }
 
@@ -45,9 +41,7 @@ public class SelftestServiceTest {
     public void test_doGet_kritiskeDeltesterOkIkkeKritiskeDeltesterFeil() {
         SelftestResultat resultat = lagSelftestResultat(true, false);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(APPLICATION_JSON, false);
-
         assertThat(response).isNotNull();
     }
 
@@ -55,9 +49,7 @@ public class SelftestServiceTest {
     public void test_doGet_kritiskeDeltesterFeilIkkeKritiskeDeltesterOk() {
         SelftestResultat resultat = lagSelftestResultat(false, true);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(APPLICATION_JSON, false);
-
         assertThat(response).isNotNull();
     }
 
@@ -65,9 +57,7 @@ public class SelftestServiceTest {
     public void test_doGet_kritiskeDeltesterFeilIkkeKritiskeDeltesterFeil() {
         SelftestResultat resultat = lagSelftestResultat(false, false);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(APPLICATION_JSON, false);
-
         assertThat(response).isNotNull();
     }
 
@@ -75,9 +65,7 @@ public class SelftestServiceTest {
     public void test_doGet_html() {
         SelftestResultat resultat = lagSelftestResultat(true, true);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(TEXT_HTML, false);
-
         assertThat(response).isNotNull();
         assertThat(response.getHeaders()).containsKeys("Content-Type");
         assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
@@ -88,24 +76,22 @@ public class SelftestServiceTest {
     public void test_doGet_jsonAsHtml() {
         SelftestResultat resultat = lagSelftestResultat(true, true);
         when(mockSelftests.run()).thenReturn(resultat);
-
         Response response = service.doSelftest(TEXT_HTML, true);
-
         assertThat(response).isNotNull();
         assertThat(response.getHeaders()).containsKeys("Content-Type");
         assertThat(response.getHeaders().get("Content-Type")).contains(TEXT_HTML);
         assertThat(response.getEntity()).isNotNull();
     }
 
-    //-------
-
     private SelftestResultat lagSelftestResultat(boolean kritiskeOk, boolean ikkeKritiskeOk) {
         SelftestResultat resultat = lagSelftestResultat();
 
-        HealthCheck.Result delRes1 = kritiskeOk ? HealthCheck.Result.healthy() : HealthCheck.Result.unhealthy(MSG_KRITISK_FEIL);
+        HealthCheck.Result delRes1 = kritiskeOk ? HealthCheck.Result.healthy()
+                : HealthCheck.Result.unhealthy(MSG_KRITISK_FEIL);
         resultat.leggTilResultatForKritiskTjeneste(delRes1);
 
-        HealthCheck.Result delRes2 = ikkeKritiskeOk ? HealthCheck.Result.healthy() : HealthCheck.Result.unhealthy(MSG_IKKEKRITISK_FEIL);
+        HealthCheck.Result delRes2 = ikkeKritiskeOk ? HealthCheck.Result.healthy()
+                : HealthCheck.Result.unhealthy(MSG_IKKEKRITISK_FEIL);
         resultat.leggTilResultatForIkkeKritiskTjeneste(delRes2);
 
         return resultat;
