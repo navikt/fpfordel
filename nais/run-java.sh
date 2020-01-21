dea4bc8fe2636e1e022982e9c45fbbf66fe89b7f
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-21 11:37:50 +0100

    revert

--- nais/run-java.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-export JAVA_OPTS="${JAVA_OPTS:-} -Djava.security.egd=file:/dev/./urandom"
-
-export STARTUP_CLASS=${STARTUP_CLASS:-"no.nav.foreldrepenger.fordel.web.server.jetty.JettyServer"}
-export CLASSPATH="app.jar:lib/*"
-
-exec java -cp ${CLASSPATH} ${DEFAULT_JAVA_OPTS:-} ${JAVA_OPTS}  -Dwebapp=${WEBAPP:-"./webapp"} -Dapplication.name=${APP_NAME} ${STARTUP_CLASS?} ${SERVER_PORT:-8080} $@
