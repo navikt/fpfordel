50834e0c35e74589269190579068d6a2062a5340
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-21 09:35:12 +0100

    flyttet oracle

---
+++ oracle/init.d/initdb.sql
@@ -0,0 +1,10 @@
+alter database set TIME_ZONE='Europe/Oslo';
+alter system set recyclebin=OFF DEFERRED;
+alter profile default limit password_life_time unlimited;
+alter system set processes=150 scope=spfile;
+alter system set session_cached_cursors=100 scope=spfile;
+alter system set session_max_open_files=100 scope=spfile;
+alter system set sessions=100 scope=spfile;
+alter system set license_max_sessions=100 scope=spfile;
+alter system set license_sessions_warning=100 scope=spfile;
+GRANT CREATE USER, CONNECT, RESOURCE, DBA, ALTER SESSION TO vl_dba WITH ADMIN OPTION;
