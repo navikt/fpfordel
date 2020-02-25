67bc5e078025f15b839ebfd382124db6ace542c7
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-02-25 16:29:48 +0100

    are you ready to be kafaesqued

---
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/KafkaProperties.java
@@ -0,0 +1,43 @@
+package no.nav.foreldrepenger.mottak.felles.kafka;
+
+import static org.apache.kafka.clients.admin.AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG;
+import static org.apache.kafka.clients.admin.AdminClientConfig.CLIENT_ID_CONFIG;
+import static org.apache.kafka.clients.admin.AdminClientConfig.SECURITY_PROTOCOL_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.common.config.SaslConfigs.SASL_JAAS_CONFIG;
+import static org.apache.kafka.common.config.SaslConfigs.SASL_MECHANISM;
+
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import no.nav.vedtak.util.env.Environment;
+
+class KafkaProperties {
+    private static final Environment ENV = Environment.current();
+
+    private KafkaProperties() {
+    }
+
+    static Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(BOOTSTRAP_SERVERS_CONFIG, ENV.getRequiredProperty(BOOTSTRAP_SERVERS_CONFIG));
+        properties.setProperty(CLIENT_ID_CONFIG, ENV.getRequiredProperty(CLIENT_ID_CONFIG));
+        properties.setProperty(SASL_JAAS_CONFIG, jaasCfg());
+        properties.setProperty(SECURITY_PROTOCOL_CONFIG, ENV.getProperty(SECURITY_PROTOCOL_CONFIG, "SASL_SSL"));
+        properties.setProperty(SASL_MECHANISM, ENV.getProperty(SASL_MECHANISM, "PLAIN"));
+        properties.setProperty(KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+
+    private static String jaasCfg() {
+        return String.format(
+                "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";",
+                ENV.getRequiredProperty("systembruker.username"),
+                ENV.getRequiredProperty("systembruker.password"));
+
+    }
+
+}
---
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/SøknadHendelseProdusent.java
@@ -0,0 +1,55 @@
+package no.nav.foreldrepenger.mottak.felles.kafka;
+
+import static no.nav.foreldrepenger.mottak.felles.kafka.KafkaProperties.properties;
+import static no.nav.vedtak.log.mdc.MDCOperations.generateCallId;
+import static no.nav.vedtak.log.mdc.MDCOperations.getCallId;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//@ApplicationScoped
+public class SøknadHendelseProdusent {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SøknadHendelseProdusent.class);
+
+    private static final String CALLID_NAME = "Nav-CallId";
+    private Producer<String, String> producer;
+    private String topic;
+
+    SøknadHendelseProdusent() {
+    }
+
+    @Inject
+    public SøknadHendelseProdusent(String topic) {
+        this.producer = new KafkaProducer<>(properties());
+        this.topic = topic;
+    }
+
+    public void send(String json, String nøkkel) {
+        producer.send(melding(json, nøkkel), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata md, Exception e) {
+                if (e == null) {
+                    LOG.info("Sendte melding {} med offset {} på {}", json, md.offset(), md.topic());
+                } else {
+                    LOG.warn("Kunne ikke sende melding {} på {}", json, md.topic(), e);
+                }
+            }
+        });
+    }
+
+    public ProducerRecord<String, String> melding(String json, String nøkkel) {
+       return new ProducerRecord<>(topic, null, nøkkel, json,
+                new RecordHeaders().add(CALLID_NAME, Optional.ofNullable(getCallId()).orElse(generateCallId()).getBytes()));
+    }
+}
