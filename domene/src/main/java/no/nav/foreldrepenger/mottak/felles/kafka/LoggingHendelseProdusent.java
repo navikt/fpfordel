be38fc9c3d13c17ee0091ab7e1b206a0d3066689
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-02-26 13:15:10 +0100

    prøv med en selector

--- domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
@@ -1,11 +1,9 @@
 package no.nav.foreldrepenger.mottak.felles.kafka;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationScoped
+//@ApplicationScoped
 public class LoggingHendelseProdusent implements HendelseProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingHendelseProdusent.class);
 
