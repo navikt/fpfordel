943943aee1df6c92ae033d8ceb335ff39ccd8ca2
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-02-26 14:45:51 +0100

    test

--- domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
@@ -1,13 +1,10 @@
 package no.nav.foreldrepenger.mottak.felles.kafka;
 
-import javax.enterprise.inject.Default;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.vedtak.util.env.Cluster;
 
-@Default
 @ConditionalOnClusters(clusters = Cluster.DEV_FSS)
 public class LoggingHendelseProdusent implements HendelseProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingHendelseProdusent.class);
