e6c9b0377848c07237eabe1b799c0f23388c9cae
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-02-26 16:36:08 +0100

    set topic

--- domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/LoggingHendelseProdusent.java
@@ -1,16 +1,28 @@
 package no.nav.foreldrepenger.mottak.felles.kafka;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@ApplicationScoped
+import no.nav.vedtak.konfig.KonfigVerdi;
+
+@Dependent
 public class LoggingHendelseProdusent implements HendelseProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingHendelseProdusent.class);
+    private final String topic;
+
+    public LoggingHendelseProdusent(@KonfigVerdi("kafka.topics.fordeling") String topic) {
+        this.topic = topic;
+    }
 
     @Override
     public void send(Object hendelse, String nøkkel) {
-        LOG.info("Publiserer hendelse {} med nøkkel {}", hendelse, nøkkel);
+        LOG.info("Publiserer hendelse {} med nøkkel {} på topic {}", hendelse, nøkkel, topic);
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "[topic=" + topic + "]";
     }
 }
