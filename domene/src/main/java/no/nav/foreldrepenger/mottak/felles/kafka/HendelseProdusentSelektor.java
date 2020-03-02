5ca2f3c6da670a4291c9b8abe5d3080c00811b11
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-02 12:59:27 +0100

    logging in t4

--- domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/HendelseProdusentSelektor.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/felles/kafka/HendelseProdusentSelektor.java
@@ -9,16 +9,16 @@ import org.slf4j.LoggerFactory;
 
 import no.nav.vedtak.util.env.Environment;
 
+@ApplicationScoped
 public class HendelseProdusentSelektor {
     private static final Logger LOG = LoggerFactory.getLogger(HendelseProdusentSelektor.class);
     private static final Environment ENV = Environment.current();
 
     @Produces
-    @ApplicationScoped
     HendelseProdusent hendelseProdusent(Instance<HendelseProdusent> instance) {
-        LOG.info("Finner hendelsesprodusent-instans i {}", ENV.namespace());
+        LOG.info("Finner hendelsesprodusent-instans i {}-{}", ENV.clusterName(), ENV.namespace());
         if ("t4".equals(ENV.namespace())) {
-            var instans = instance.select(LoggingHendelseProdusent.class).get();
+            var instans = instance.select(LoggingHendelseProdusent.class);
             LOG.info("Bruker logging-instans {} i {}-{}", instans.getClass().getSimpleName(), ENV.clusterName(),
                     ENV.namespace());
         }
