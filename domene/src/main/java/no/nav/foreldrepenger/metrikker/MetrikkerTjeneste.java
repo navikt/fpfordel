141e1a16ed6f4386cd5bfe2c53a655291722830c
Michal J. Sladek <michal.sladek@nav.no>
2020-04-21 10:54:30 +0200

    Legger til tjenesten i TaskStatusEventObserver.

--- domene/src/main/java/no/nav/foreldrepenger/metrikker/MetrikkerTjeneste.java
+++ domene/src/main/java/no/nav/foreldrepenger/metrikker/MetrikkerTjeneste.java
@@ -1,5 +1,7 @@
 package no.nav.foreldrepenger.metrikker;
 
+import java.util.Map;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -13,5 +15,14 @@ public class MetrikkerTjeneste {
     @Inject
     public MetrikkerTjeneste(SensuKlient sensuKlient) {
         this.sensuKlient = sensuKlient;
+    }
+
+    public void logFeilProsessTaskEvent(String prosessTaskType, int antall) {
+        final SensuEvent sensuEvent = SensuEvent.createSensuEvent(
+                "antall_feilende_prosesstask",
+                Map.of("prosesstask_type", prosessTaskType),
+                Map.of("antall", antall));
+
+        sensuKlient.logMetrics(sensuEvent);
     }
 }
--- domene/src/main/java/no/nav/foreldrepenger/metrikker/TaskStatusEventObserver.java
+++ domene/src/main/java/no/nav/foreldrepenger/metrikker/TaskStatusEventObserver.java
@@ -1,7 +1,5 @@
 package no.nav.foreldrepenger.metrikker;
 
-import java.util.Map;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
@@ -12,37 +10,30 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskStatus;
 @ApplicationScoped
 class TaskStatusEventObserver {
 
-    private SensuKlient sensuKlient;
+    private MetrikkerTjeneste metrikkerTjeneste;
 
     TaskStatusEventObserver() {
     }
 
     @Inject
-    TaskStatusEventObserver(SensuKlient sensuKlient) {
-        this.sensuKlient = sensuKlient;
+    public TaskStatusEventObserver(MetrikkerTjeneste metrikkerTjeneste) {
+        this.metrikkerTjeneste = metrikkerTjeneste;
     }
 
     void observerProsessTasks(@Observes ProsessTaskEvent event) {
+        // Gjentaker - ignorer
         if (event.getGammelStatus() != null
                 && ProsessTaskStatus.FEILET.equals(event.getGammelStatus())
                 && ProsessTaskStatus.FEILET.equals(event.getNyStatus())) {
             return;
         }
+        // Feiler ikke lenger
         if (event.getGammelStatus() != null && ProsessTaskStatus.FEILET.equals(event.getGammelStatus())) {
-            final SensuEvent sensuEvent = SensuEvent.createSensuEvent(
-                    "k9-fordel.antall_feilende_prosesstask",
-                    Map.of("prosesstask_type", event.getTaskType()),
-                    Map.of("antall", -1));
-
-            sensuKlient.logMetrics(sensuEvent);
+            metrikkerTjeneste.logFeilProsessTaskEvent(event.getTaskType(), -1);
         }
+        // Ny feil
         if (ProsessTaskStatus.FEILET.equals(event.getNyStatus())) {
-            final SensuEvent sensuEvent = SensuEvent.createSensuEvent(
-                    "k9-fordel.antall_feilende_prosesstask",
-                    Map.of("prosesstask_type", event.getTaskType()),
-                    Map.of("antall", 1));
-
-            sensuKlient.logMetrics(sensuEvent);
+            metrikkerTjeneste.logFeilProsessTaskEvent(event.getTaskType(), 1);
         }
     }
 }
