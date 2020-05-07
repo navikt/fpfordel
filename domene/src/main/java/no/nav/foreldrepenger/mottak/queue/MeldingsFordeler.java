dfd367fbc953db6b7a0fa158a51f246fb651a424
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2020-05-07 12:33:09 +0200

    NOJIRA fjerne mq fra fpfordel

--- domene/src/main/java/no/nav/foreldrepenger/mottak/queue/MeldingsFordeler.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foreldrepenger.mottak.queue;
-
-import no.nav.melding.virksomhet.dokumentnotifikasjon.v1.XMLForsendelsesinformasjon;
-
-public interface MeldingsFordeler {
-
-    void execute(String correlationId, XMLForsendelsesinformasjon forsendelsesinfo);
-}
