1d3edb896f2443f9ada768537f8a664b7d11fdd3
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-09-07 14:10:41 +0200

    slett ubrukt

--- domene/src/main/java/no/nav/foreldrepenger/sikkerhet/abac/BeskyttetRessursAttributt.java
+++ domene/src/main/java/no/nav/foreldrepenger/sikkerhet/abac/BeskyttetRessursAttributt.java
@@ -1,16 +1,6 @@
 package no.nav.foreldrepenger.sikkerhet.abac;
 
 public class BeskyttetRessursAttributt {
-    public static final String APPLIKASJON = "no.nav.abac.attributter.foreldrepenger";
     public static final String FAGSAK = "no.nav.abac.attributter.foreldrepenger.fagsak";
-    public static final String VENTEFRIST = "no.nav.abac.attributter.foreldrepenger.fagsak.ventefrist";
     public static final String DRIFT = "no.nav.abac.attributter.foreldrepenger.drift";
-    public static final String BATCH = "no.nav.abac.attributter.foreldrepenger.batch";
-    public static final String SAKLISTE = "no.nav.abac.attributter.foreldrepenger.sakliste";
-    public static final String OPPGAVEKO = "no.nav.abac.attributter.foreldrepenger.oppgaveko";
-    public static final String OPPGAVESTYRING = "no.nav.abac.attributter.foreldrepenger.oppgavestyring";
-    public static final String PIP = "pip.tjeneste.kan.kun.kalles.av.pdp.servicebruker";
-    public static final String OPPGAVESTYRING_AVDELINGENHET = "no.nav.abac.attributter.foreldrepenger.oppgavestyring.avdelingsenhet";
-    public static final String RISIKOKLASSIFISERING = "no.nav.abac.attributter.foreldrepenger.risikoklassifisering";
-    public static final String UTTAKSPLAN = "no.nav.abac.attributter.resource.foreldrepenger.uttaksplan";
 }
