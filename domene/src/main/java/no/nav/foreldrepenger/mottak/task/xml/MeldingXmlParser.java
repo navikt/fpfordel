e0181fb0f1bb0a7989fe0fe1ec13b20e8edbc05c
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-09-08 09:54:10 +0200

    moderne java

--- domene/src/main/java/no/nav/foreldrepenger/mottak/task/xml/MeldingXmlParser.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/task/xml/MeldingXmlParser.java
@@ -1,68 +1,64 @@
 package no.nav.foreldrepenger.mottak.task.xml;
 
+import static no.nav.foreldrepenger.mottak.task.xml.MeldingXmlParserFeil.FACTORY;
+import static no.nav.vedtak.felles.integrasjon.felles.ws.JaxbHelper.unmarshalAndValidateXMLWithStAX;
+import static no.nav.vedtak.felles.integrasjon.felles.ws.JaxbHelper.unmarshalXMLWithStAX;
 import static no.nav.vedtak.felles.xml.XmlUtils.retrieveNameSpaceOfXML;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
-import javax.xml.bind.JAXBException;
 import javax.xml.stream.XMLStreamException;
 
-import org.xml.sax.SAXException;
-
 import no.nav.foreldrepenger.mottak.domene.MottattStrukturertDokument;
-import no.nav.vedtak.felles.integrasjon.felles.ws.JaxbHelper;
 import no.seres.xsd.nav.inntektsmelding_m._201809.InntektsmeldingConstants;
 
 public final class MeldingXmlParser {
 
-    private static final Map<String, UnmarshallFunction> UNMARSHALL_FUNCTIONS;
-
-    static {
-        Map<String, UnmarshallFunction> map = new HashMap<>();
+    private static final Map<String, UnmarshallFunction> UNMARSHALL_FUNCTIONS = Map.of(
 
 //        TODO: Legge inn og erstatt eksisterende innslag
-//              slik at søknadsXML blir validert ved parseing.
-        map.put(no.nav.foreldrepenger.søknad.v3.SøknadConstants.NAMESPACE, (xml) -> JaxbHelper.unmarshalXMLWithStAX(no.nav.foreldrepenger.søknad.v3.SøknadConstants.JAXB_CLASS,
-                xml,
-                no.nav.foreldrepenger.søknad.v3.SøknadConstants.ADDITIONAL_CLASSES));
+//              slik at søknadsXML blir validert ved parsing.
+            no.nav.foreldrepenger.søknad.v3.SøknadConstants.NAMESPACE,
+            (xml) -> unmarshalXMLWithStAX(no.nav.foreldrepenger.søknad.v3.SøknadConstants.JAXB_CLASS, xml,
+                    no.nav.foreldrepenger.søknad.v3.SøknadConstants.ADDITIONAL_CLASSES),
 
-        map.put(InntektsmeldingConstants.NAMESPACE,
-                (xml) -> JaxbHelper.unmarshalAndValidateXMLWithStAX(InntektsmeldingConstants.JAXB_CLASS, xml,
-                        InntektsmeldingConstants.XSD_LOCATION));
+            InntektsmeldingConstants.NAMESPACE,
+            (xml) -> unmarshalAndValidateXMLWithStAX(InntektsmeldingConstants.JAXB_CLASS, xml,
+                    InntektsmeldingConstants.XSD_LOCATION),
 
-        map.put(no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.NAMESPACE,
-                (xml) -> JaxbHelper.unmarshalAndValidateXMLWithStAX(no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.JAXB_CLASS, xml,
-                        no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.XSD_LOCATION));
-
-
-
-        UNMARSHALL_FUNCTIONS = Collections.unmodifiableMap(map);
-    }
+            no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.NAMESPACE,
+            (xml) -> unmarshalAndValidateXMLWithStAX(no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.JAXB_CLASS,
+                    xml,
+                    no.seres.xsd.nav.inntektsmelding_m._201812.InntektsmeldingConstants.XSD_LOCATION));
 
     private MeldingXmlParser() {
     }
 
-    @SuppressWarnings("rawtypes")
-    public static MottattStrukturertDokument unmarshallXml(String xml) {
-        String nameSpaceOfXML = "ukjent";
+    public static MottattStrukturertDokument<?> unmarshallXml(String xml) {
+        return Optional.ofNullable(UNMARSHALL_FUNCTIONS.get(ns(xml)))
+                .map(f -> {
+                    try {
+                        return f.parse(xml);
+                    } catch (Exception e) {
+                        throw FACTORY.uventetFeilVedParsingAvXml(ns(xml), e).toException();
+                    }
+                })
+                .map(MottattStrukturertDokument::toXmlWrapper)
+                .orElseThrow(() -> FACTORY.ukjentNamespace(ns(xml), new IllegalStateException())
+                        .toException());
+
+    }
+
+    private static String ns(String xml) {
         try {
-            nameSpaceOfXML = retrieveNameSpaceOfXML(xml);
-            UnmarshallFunction unmarshallFunction = UNMARSHALL_FUNCTIONS.get(nameSpaceOfXML);
-            if (unmarshallFunction != null) {
-                Object jaxbObjekt = unmarshallFunction.parse(xml);
-                return MottattStrukturertDokument.toXmlWrapper(jaxbObjekt);
-            } else {
-                throw MeldingXmlParserFeil.FACTORY.ukjentNamespace(nameSpaceOfXML, new IllegalStateException())
-                        .toException();
-            }
-        } catch (XMLStreamException | JAXBException | SAXException e) {
-            throw MeldingXmlParserFeil.FACTORY.uventetFeilVedParsingAvXml(nameSpaceOfXML, e).toException();
+            return retrieveNameSpaceOfXML(xml);
+        } catch (XMLStreamException e) {
+            return null;
         }
     }
 
     private interface UnmarshallFunction {
-        Object parse(String xml) throws JAXBException, SAXException, XMLStreamException;
+        Object parse(String xml) throws Exception;
     }
 }
