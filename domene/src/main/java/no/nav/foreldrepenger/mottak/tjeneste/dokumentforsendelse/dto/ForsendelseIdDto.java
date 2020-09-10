358206c5bfed6b6dd3d2b2c5a103844d65380ff4
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-09-10 11:40:35 +0200

    fjernet tøys

--- domene/src/main/java/no/nav/foreldrepenger/mottak/tjeneste/dokumentforsendelse/dto/ForsendelseIdDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/tjeneste/dokumentforsendelse/dto/ForsendelseIdDto.java
@@ -4,8 +4,6 @@ import java.util.UUID;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import no.nav.foreldrepenger.sikkerhet.abac.AppAbacAttributtType;
 import no.nav.vedtak.sikkerhet.abac.AbacDataAttributter;
@@ -13,19 +11,15 @@ import no.nav.vedtak.sikkerhet.abac.AbacDto;
 
 public class ForsendelseIdDto implements AbacDto {
 
-    public static final String UUID_REGEXP = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
-
     @NotNull
-    @Pattern(regexp = UUID_REGEXP)
-    @Size(max = 36)
-    private final String forsendelseId;
+    private UUID forsendelseId;
 
     public ForsendelseIdDto(@Valid String forsendelseId) {
-        this.forsendelseId = forsendelseId;
+        this.forsendelseId = UUID.fromString(forsendelseId);
     }
 
     public UUID getForsendelseId() {
-        return UUID.fromString(forsendelseId);
+        return forsendelseId;
     }
 
     @Override
