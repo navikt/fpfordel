516164c611482aae08d48be61d287fa507b3bd3c
Espen Waaga <espen.waaga@nav.no>
2021-01-15 14:34:57 +0100

    FIX: VariantFormat i Journalpost må ha små bokstaver for deseralisering.

--- domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/model/Dokumentvariant.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/model/Dokumentvariant.java
@@ -3,6 +3,6 @@ package no.nav.foreldrepenger.mottak.journal.saf.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Dokumentvariant(VariantFormat variantFormat) {
+public record Dokumentvariant(VariantFormat variantformat) {
 
 }
