c75c828e32b80ad92a44d3af2d3043bea14fbc11
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-19 14:44:38 +0100

    no jersey

--- domene/src/main/java/no/nav/foreldrepenger/mottak/person/PersonTjeneste.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/person/PersonTjeneste.java
@@ -13,7 +13,6 @@ import java.util.function.Predicate;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,7 +58,7 @@ public class PersonTjeneste implements PersonInformasjon {
     }
 
     @Inject
-    public PersonTjeneste(@Named("jersey") Pdl pdl) {
+    public PersonTjeneste(/* @Named("jersey") */ Pdl pdl) {
         this.pdl = pdl;
         this.cacheAktørIdTilIdent = cache(DEFAULT_CACHE_SIZE, DEFAULT_CACHE_TIMEOUT_HOURS, HOURS);
         this.cacheIdentTilAktørId = cache(DEFAULT_CACHE_SIZE, DEFAULT_CACHE_TIMEOUT_HOURS, HOURS);
