c8103bc34c043663511c7113ddeb62ce167806e9
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-02-05 10:04:53 +0100

    no scope

--- domene/src/test/java/no/nav/foreldrepenger/mottak/task/LoggingHendelseProdusent.java
+++ domene/src/test/java/no/nav/foreldrepenger/mottak/task/LoggingHendelseProdusent.java
@@ -1,14 +1,11 @@
 package no.nav.foreldrepenger.mottak.task;
 
-import javax.enterprise.context.ApplicationScoped;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.mottak.felles.kafka.HendelseProdusent;
 
-@ApplicationScoped
-public class LoggingHendelseProdusent implements HendelseProdusent {
+class LoggingHendelseProdusent implements HendelseProdusent {
     private static final Logger LOG = LoggerFactory.getLogger(LoggingHendelseProdusent.class);
 
     @Override
