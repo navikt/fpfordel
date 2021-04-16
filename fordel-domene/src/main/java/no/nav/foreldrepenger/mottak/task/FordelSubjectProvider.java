5d860586f7ba30c803ceefda1aeca28b50d71413
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-04-16 09:15:18 +0200

    log

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/task/FordelSubjectProvider.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/task/FordelSubjectProvider.java
@@ -2,6 +2,9 @@ package no.nav.foreldrepenger.mottak.task;
 
 import javax.enterprise.context.ApplicationScoped;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.nimbusds.jwt.SignedJWT;
 
 import no.nav.vedtak.felles.prosesstask.impl.SubjectProvider;
@@ -9,13 +12,19 @@ import no.nav.vedtak.sikkerhet.context.SubjectHandler;
 
 @ApplicationScoped
 public class FordelSubjectProvider implements SubjectProvider {
+    private static final Logger LOG = LoggerFactory.getLogger(FordelSubjectProvider.class);
 
     @Override
     public String getUserIdentity() {
         try {
-            return SignedJWT.parse(SubjectHandler.getSubjectHandler().getInternSsoToken()).getJWTClaimsSet().getSubject();
+            LOG.trace("Henter id fra token");
+            var id = SignedJWT.parse(SubjectHandler.getSubjectHandler().getInternSsoToken()).getJWTClaimsSet().getSubject();
+            LOG.trace("Id fra token er {}", id);
+            return id;
         } catch (Exception e) {
-            throw new IllegalArgumentException("Kunne ikke hente brukeridentitet", e);
+            LOG.warn("Id fra token kunne ikke hentes", e);
+            return ("FIXME");
+            // throw new IllegalArgumentException("Kunne ikke hente brukeridentitet", e);
         }
     }
 }
