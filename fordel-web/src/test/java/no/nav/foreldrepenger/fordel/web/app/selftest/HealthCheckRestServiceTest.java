972c3e59a670602d587617c2843e7d6139a6f638
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-04-22 16:07:11 +0200

    fix test

--- fordel-web/src/test/java/no/nav/foreldrepenger/fordel/web/app/selftest/HealthCheckRestServiceTest.java
+++ fordel-web/src/test/java/no/nav/foreldrepenger/fordel/web/app/selftest/HealthCheckRestServiceTest.java
@@ -1,11 +1,12 @@
 package no.nav.foreldrepenger.fordel.web.app.selftest;
 
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.OK;
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
-
-import javax.ws.rs.core.Response;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,40 +35,29 @@ public class HealthCheckRestServiceTest {
     @Test
     public void test_isAlive_skal_returnere_status_200() {
         when(kafka.isAlive()).thenReturn(true);
-        assertThat(restTjeneste.isAlive().getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(restTjeneste.isAlive().getStatus()).isEqualTo(OK.getStatusCode());
     }
 
     @Test
     public void test_isReady_skal_returnere_service_unavailable_når_kritiske_selftester_feiler() {
         when(kafka.isAlive()).thenReturn(false);
-        Response responseReady = restTjeneste.isReady();
-        Response responseAlive = restTjeneste.isAlive();
-
-        assertThat(responseReady.getStatus()).isEqualTo(Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
-        assertThat(responseAlive.getStatus()).isEqualTo(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode());
+        assertThat(restTjeneste.isReady().getStatus()).isEqualTo(SERVICE_UNAVAILABLE.getStatusCode());
+        assertThat(restTjeneste.isAlive().getStatus()).isEqualTo(INTERNAL_SERVER_ERROR.getStatusCode());
     }
 
     @Test
     public void test_isReady_skal_returnere_status_delvis_når_db_feiler() {
         when(kafka.isAlive()).thenReturn(true);
         when(db.isReady()).thenReturn(false);
-
-        Response responseReady = restTjeneste.isReady();
-        Response responseAlive = restTjeneste.isAlive();
-
-        assertThat(responseReady.getStatus()).isEqualTo(Response.Status.SERVICE_UNAVAILABLE.getStatusCode());
-        assertThat(responseAlive.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(restTjeneste.isReady().getStatus()).isEqualTo(SERVICE_UNAVAILABLE.getStatusCode());
+        assertThat(restTjeneste.isAlive().getStatus()).isEqualTo(OK.getStatusCode());
     }
 
     @Test
     public void test_isReady_skal_returnere_status_ok_når_selftester_er_ok() {
         when(kafka.isAlive()).thenReturn(true);
         when(db.isReady()).thenReturn(true);
-
-        Response responseReady = restTjeneste.isReady();
-        Response responseAlive = restTjeneste.isAlive();
-
-        assertThat(responseReady.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
-        assertThat(responseAlive.getStatus()).isEqualTo(Response.Status.OK.getStatusCode());
+        assertThat(restTjeneste.isReady().getStatus()).isEqualTo(OK.getStatusCode());
+        assertThat(restTjeneste.isAlive().getStatus()).isEqualTo(OK.getStatusCode());
     }
 }
