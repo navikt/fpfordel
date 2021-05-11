12d9bcbb9689611cb99dd664a41128b6af289ebd
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-11 14:46:47 +0200

    velkommen til 2021

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/DokumentforsendelseDto.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/DokumentforsendelseDto.java
@@ -8,91 +8,11 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class DokumentforsendelseDto {
-    @NotNull
-    private UUID forsendelsesId;
-    @NotNull
-    @Size(min = 1)
-    private List<FilMetadataDto> filer;
-
-    private String saksnummer;
-
-    private LocalDateTime forsendelseMottatt;
-
-    private String brukerId;
-
-    public DokumentforsendelseDto(UUID forsendelsesId, List<FilMetadataDto> filer) {
-        this.forsendelsesId = forsendelsesId;
-        this.filer = filer;
-    }
-
-    DokumentforsendelseDto() { // NOSONAR ... is marked "javax.validation.constraints.NotNull" but is not
-                               // initialized in this constructor.
-        // For Jackson
-    }
-
-    public UUID getForsendelsesId() {
-        return forsendelsesId;
-    }
-
-    public List<FilMetadataDto> getFiler() {
-        return filer;
-    }
-
-    public String getSaksnummer() {
-        return saksnummer;
-    }
-
-    public void setSaksnummer(String saksnummer) {
-        this.saksnummer = saksnummer;
-    }
-
-    public LocalDateTime getForsendelseMottatt() {
-        return forsendelseMottatt;
-    }
-
-    public DokumentforsendelseDto setForsendelseMottatt(LocalDateTime forsendelseMottatt) {
-        this.forsendelseMottatt = forsendelseMottatt;
-        return this;
-    }
-
-    public String getBrukerId() {
-        return brukerId;
-    }
-
-    public DokumentforsendelseDto setBrukerId(String brukerId) {
-        this.brukerId = brukerId;
-        return this;
-    }
+public record DokumentforsendelseDto(@NotNull UUID forsendelsesId, String saksnummer, String brukerId, LocalDateTime forsendelseMottatt,
+        @NotNull @Size(min = 1) List<FilMetadataDto> filer) {
 }
 
-class FilMetadataDto {
-    @NotNull
-    @Pattern(regexp = "^[a-zA-Z0-9]{4}$")
-    @JsonProperty("Content-ID")
-    private String contentId;
-    @NotNull
-    @Size(min = 7, max = 7)
-    @Pattern(regexp = "^I[0-9]{6}$")
-    private String dokumentTypeId;
+record FilMetadataDto(@NotNull @Pattern(regexp = "^[a-zA-Z0-9]{4}$") String contentId,
+        @NotNull @Size(min = 7, max = 7) @Pattern(regexp = "^I[0-9]{6}$") String dokumentTypeId) {
 
-    public FilMetadataDto(String contentId, String dokumentTypeId) {
-        this.contentId = contentId;
-        this.dokumentTypeId = dokumentTypeId;
-    }
-
-    FilMetadataDto() { // NOSONAR ... is marked "javax.validation.constraints.NotNull" but is not
-                       // initialized in this constructor.
-        // For Jackson
-    }
-
-    public String getContentId() {
-        return contentId;
-    }
-
-    public String getDokumentTypeId() {
-        return dokumentTypeId;
-    }
 }
---
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/server/abac/Tuple.java
@@ -0,0 +1,5 @@
+package no.nav.foreldrepenger.fordel.web.server.abac;
+
+record Tuple(String element1, String element2) {
+
+}
