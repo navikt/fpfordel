dc31f75b774b43c6862d0259054f1a3023e904ee
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-12 20:00:07 +0200

    bump

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/model/Bruker.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/model/Bruker.java
@@ -2,6 +2,10 @@ package no.nav.foreldrepenger.mottak.journal.saf.model;
 
 public record Bruker(String id, BrukerIdType type) {
 
+    public Bruker mutate(String nyId) {
+        return new Bruker(nyId, type);
+    }
+
     public boolean erAktoerId() {
         return BrukerIdType.AKTOERID.equals(type);
     }
