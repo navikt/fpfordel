dc1d7e0df3bc20a42a65525dc165220191508d71
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-21 10:04:30 +0200

    bump

--- fordel-domene/src/main/java/no/nav/foreldrepenger/pip/PipRepository.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/pip/PipRepository.java
@@ -1,46 +1,38 @@
 package no.nav.foreldrepenger.pip;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.UUID;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
-@ApplicationScoped
+@Dependent
 public class PipRepository {
 
-    private static String PIP_QUERY = """ 
-                select BRUKER_ID 
-                from DOKUMENT_METADATA 
+    private static String PIP_QUERY = """
+                select BRUKER_ID
+                from DOKUMENT_METADATA
                 where FORSENDELSE_ID in (:dokumentforsendelseIder)
                 """;
 
-    private EntityManager entityManager;
-
-    public PipRepository() {
-    }
+    private final EntityManager entityManager;
 
     @Inject
     public PipRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
 
-    @SuppressWarnings("unchecked")
     public Set<String> hentAktørIdForForsendelser(Set<UUID> dokumentforsendelseIder) {
         Objects.requireNonNull(dokumentforsendelseIder, "dokumentforsendelseIder");
 
         if (dokumentforsendelseIder.isEmpty()) {
-            return Collections.emptySet();
+            return Set.of();
         }
 
-        Query query = entityManager.createNativeQuery(PIP_QUERY);
-        query.setParameter("dokumentforsendelseIder", dokumentforsendelseIder);
-
-        return new HashSet<>(query.getResultList());
+        return new HashSet<String>(entityManager.createNativeQuery(PIP_QUERY)
+                .setParameter("dokumentforsendelseIder", dokumentforsendelseIder).getResultList());
     }
 }
