6baf0b27e8157c875e67a637ced0b34493730913
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-25 20:45:50 +0200

    ikke generer prometheus format selv

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
@@ -1,14 +1,12 @@
 package no.nav.foreldrepenger.fordel.web.app.metrics;
 
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 import static no.nav.vedtak.felles.integrasjon.rest.jersey.AbstractJerseyRestClient.REGISTRY;
-
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
@@ -17,7 +15,6 @@ import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
 import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.swagger.v3.oas.annotations.Operation;
-import no.nav.vedtak.felles.integrasjon.rest.jersey.AbstractJerseyRestClient;
 
 @Path("/metrics")
 @ApplicationScoped
@@ -34,17 +31,11 @@ public class PrometheusRestService {
     @GET
     @Operation(tags = "metrics", hidden = true)
     @Path("/prometheus")
+    @Produces(TEXT_PLAIN)
     public Response prometheus() {
-        try (final Writer writer = new StringWriter()) {
-            // TextFormatter.write004(writer,
-            // CollectorRegistry.defaultRegistry.metricFamilySamples());
-            // return
-            // Response.ok().encoding("UTF-8").entity(writer.toString()).header("content-type",
-            // TextFormatter.CONTENT_TYPE_004).build();
-            return Response.ok().encoding("UTF-8").entity(AbstractJerseyRestClient.REGISTRY.scrape())
-                    .header("content-type", TextFormatter.CONTENT_TYPE_004).build();
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+        return Response.ok().encoding("UTF-8")
+                .entity(REGISTRY.scrape())
+                .build();
+
     }
 }
