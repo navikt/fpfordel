bd2daa14f9def3d8bdbf9e4c06c7ff7bb1993a54
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-28 09:36:14 +0200

    scrape

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
@@ -1,7 +1,7 @@
 package no.nav.foreldrepenger.fordel.web.app.metrics;
 
-import static io.micrometer.core.instrument.Metrics.globalRegistry;
 import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static no.nav.vedtak.log.metrics.MetricsUtil.REGISTRY;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
@@ -9,25 +9,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
-import io.micrometer.core.instrument.binder.jvm.JvmThreadMetrics;
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
-import io.micrometer.prometheus.PrometheusMeterRegistry;
 import io.swagger.v3.oas.annotations.Operation;
 
 @Path("/metrics")
 @ApplicationScoped
 public class PrometheusRestService {
-
-    static {
-        new ClassLoaderMetrics().bindTo(globalRegistry);
-        new JvmMemoryMetrics().bindTo(globalRegistry);
-        new JvmGcMetrics().bindTo(globalRegistry);
-        new ProcessorMetrics().bindTo(globalRegistry);
-        new JvmThreadMetrics().bindTo(globalRegistry);
-    }
 
     @GET
     @Operation(tags = "metrics", hidden = true)
@@ -35,14 +21,7 @@ public class PrometheusRestService {
     @Produces(TEXT_PLAIN)
     public Response prometheus() {
         return Response.ok()
-                .entity(globalRegistry.getRegistries()
-                        .stream()
-                        .filter(PrometheusMeterRegistry.class::isInstance)
-                        .findFirst()
-                        .map(PrometheusMeterRegistry.class::cast)
-                        .map(PrometheusMeterRegistry::scrape)
-                        .orElseThrow())
+                .entity(REGISTRY.scrape())
                 .build();
-
     }
 }
