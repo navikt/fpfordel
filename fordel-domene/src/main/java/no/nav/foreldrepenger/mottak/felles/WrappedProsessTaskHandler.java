859a8d6556c66092b6b7ebe324e4d03791b95fce
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-05-31 13:42:56 +0200

    filtrer vekk ids fra path

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
@@ -2,7 +2,6 @@ package no.nav.foreldrepenger.mottak.felles;
 
 import static no.nav.vedtak.log.mdc.MDCOperations.ensureCallId;
 
-import io.micrometer.core.instrument.Metrics;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
@@ -25,20 +24,12 @@ public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, C
         ensureCallId();
         var w = new MottakMeldingDataWrapper(data);
         precondition(w);
-        time(data, w);
-        Object neste;
+        var neste = doTask(w);
         if (neste != null) {
             postcondition(neste);
             var taskdata = neste.getProsessTaskData();
             taskdata.setCallIdFraEksisterende();
             prosessTaskRepository.lagre(taskdata);
         }
-    }
-
-    private MottakMeldingDataWrapper time(ProsessTaskData data, MottakMeldingDataWrapper w) {
-        final MottakMeldingDataWrapper neste;
-        Metrics.timer("task.time", "type", data.getTaskType()).record(() -> {
-            neste = doTask(w);
-        });
     }
 }
