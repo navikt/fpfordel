03123c0990c88dcfc156f0e67a1dabb9289d0490
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-02 14:42:46 +0200

    cleanup

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
@@ -16,6 +16,10 @@ public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, C
 
     protected ProsessTaskRepository prosessTaskRepository;
 
+    public WrappedProsessTaskHandler() {
+
+    }
+
     public WrappedProsessTaskHandler(ProsessTaskRepository repo) {
         this.prosessTaskRepository = repo;
     }
