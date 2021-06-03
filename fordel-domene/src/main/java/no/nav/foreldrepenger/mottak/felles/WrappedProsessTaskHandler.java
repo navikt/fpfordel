642a87f706ced2f2d65b25f4f2f03ef5ec69a3ca
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-03 11:54:36 +0200

    renamed for testing

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
@@ -10,7 +10,7 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
 
 public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, Conditions {
     private static final String TYPE = "type";
-    private static final String FORDEL_TASK_TIME = "tasks";
+    private static final String FORDEL_TASK_TIME = "tasks1";
 
     protected abstract MottakMeldingDataWrapper doTask(MottakMeldingDataWrapper w);
 
