458716b7edf53bc3bc74e55e0b4d0d47bd043e26
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-03 12:57:08 +0200

    simplify

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/metrics/PrometheusRestService.java
@@ -7,7 +7,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Response;
 
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -19,9 +18,7 @@ public class PrometheusRestService {
     @Operation(tags = "metrics", hidden = true)
     @Path("/prometheus")
     @Produces(TEXT_PLAIN)
-    public Response prometheus() {
-        return Response.ok()
-                .entity(REGISTRY.scrape())
-                .build();
+    public String prometheus() {
+        return REGISTRY.scrape();
     }
 }
