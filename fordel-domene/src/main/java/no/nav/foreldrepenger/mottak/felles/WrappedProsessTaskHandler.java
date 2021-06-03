9dce0793cb95b4e1604dc1ac4b0804baabf6e207
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-03 14:55:35 +0200

    histogram

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
@@ -1,20 +1,34 @@
 package no.nav.foreldrepenger.mottak.felles;
 
-import static io.micrometer.core.instrument.Metrics.timer;
 import static no.nav.vedtak.log.mdc.MDCOperations.ensureCallId;
 
-import no.nav.vedtak.exception.VLException;
+import io.micrometer.core.instrument.Meter.Id;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
 
 public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, Conditions {
-    private static final String TYPE = "type";
-    private static final String FORDEL_TASK_TIME = "tasks";
 
     protected abstract MottakMeldingDataWrapper doTask(MottakMeldingDataWrapper w);
 
     protected ProsessTaskRepository prosessTaskRepository;
+
+    static {
+        Metrics.globalRegistry.config().meterFilter(new MeterFilter() {
+            @Override
+            public DistributionStatisticConfig configure(Id id, DistributionStatisticConfig config) {
+                if (id.getName().startsWith("task")) {
+                    return DistributionStatisticConfig.builder()
+                            .percentilesHistogram(true)
+                            .percentiles(0.5, 0.95).build().merge(config);
+                }
+                return config;
+            }
+        });
+    }
 
     public WrappedProsessTaskHandler() {
 
@@ -29,18 +43,13 @@ public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, C
         ensureCallId();
         var w = new MottakMeldingDataWrapper(data);
         precondition(w);
-        try {
-            var neste = timer(FORDEL_TASK_TIME, TYPE, data.getTaskType()).recordCallable(() -> doTask(w));
-            if (neste != null) {
-                postcondition(neste);
-                var taskdata = neste.getProsessTaskData();
-                taskdata.setCallIdFraEksisterende();
-                prosessTaskRepository.lagre(taskdata);
-            }
-        } catch (VLException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e);
+        var neste = doTask(w);
+        if (neste != null) {
+            postcondition(neste);
+            var taskdata = neste.getProsessTaskData();
+            taskdata.setCallIdFraEksisterende();
+            prosessTaskRepository.lagre(taskdata);
         }
+
     }
 }
