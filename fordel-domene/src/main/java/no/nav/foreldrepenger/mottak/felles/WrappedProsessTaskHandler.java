964a0a89f7c10a286594eade5c83bf72cf5d3511
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-04 20:43:36 +0200

    bump

--- fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
+++ fordel-domene/src/main/java/no/nav/foreldrepenger/mottak/felles/WrappedProsessTaskHandler.java
@@ -2,10 +2,6 @@ package no.nav.foreldrepenger.mottak.felles;
 
 import static no.nav.vedtak.log.mdc.MDCOperations.ensureCallId;
 
-import io.micrometer.core.instrument.Meter.Id;
-import io.micrometer.core.instrument.Metrics;
-import io.micrometer.core.instrument.config.MeterFilter;
-import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskRepository;
@@ -15,20 +11,6 @@ public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, C
     protected abstract MottakMeldingDataWrapper doTask(MottakMeldingDataWrapper w);
 
     protected ProsessTaskRepository prosessTaskRepository;
-
-    static {
-        Metrics.globalRegistry.config().meterFilter(new MeterFilter() {
-            @Override
-            public DistributionStatisticConfig configure(Id id, DistributionStatisticConfig config) {
-                if (id.getName().startsWith("task")) {
-                    return DistributionStatisticConfig.builder()
-                            .percentilesHistogram(true)
-                            .percentiles(0.5, 0.95, 0.99).build().merge(config);
-                }
-                return config;
-            }
-        });
-    }
 
     public WrappedProsessTaskHandler() {
 
@@ -50,6 +32,5 @@ public abstract class WrappedProsessTaskHandler implements ProsessTaskHandler, C
             taskdata.setCallIdFraEksisterende();
             prosessTaskRepository.lagre(taskdata);
         }
-
     }
 }
