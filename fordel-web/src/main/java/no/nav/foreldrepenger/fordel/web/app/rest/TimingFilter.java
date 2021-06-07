b6ae8acc0532e05294d2a053423d4b666ff27eba
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-07 18:17:08 +0200

    time

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/TimingFilter.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/TimingFilter.java
@@ -1,5 +1,8 @@
 package no.nav.foreldrepenger.fordel.web.app.rest;
 
+import static io.micrometer.core.instrument.Metrics.timer;
+import static no.nav.vedtak.log.metrics.MetricsUtil.utvidMedHistogram;
+
 import java.io.IOException;
 import java.time.Duration;
 
@@ -9,28 +12,19 @@ import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.ext.Provider;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.micrometer.core.instrument.Metrics;
-import no.nav.vedtak.log.metrics.MetricsUtil;
-
 @Provider
 public class TimingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private static final String METRIC_NAME = "rest";
-    private static final Logger LOG = LoggerFactory.getLogger(TimingFilter.class);
     private static final ThreadLocalTimer TIMER = new ThreadLocalTimer();
 
     public TimingFilter() {
-        MetricsUtil.utvidMedHistogram(METRIC_NAME);
+        utvidMedHistogram(METRIC_NAME);
     }
 
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
-        var stop = TIMER.stop();
-        Metrics.timer(METRIC_NAME, "path", req.getUriInfo().getPath()).record(Duration.ofMillis(stop));
-        LOG.info("Eksekvering {} tok {}ms", req.getUriInfo().getPath(), stop);
+        timer(METRIC_NAME, "path", req.getUriInfo().getPath()).record(Duration.ofMillis(TIMER.stop()));
     }
 
     @Override
