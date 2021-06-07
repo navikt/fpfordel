ecf28aa673211e71dece9d6e0dae081170ae0114
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-07 18:15:18 +0200

    time

--- fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/TimingFilter.java
+++ fordel-web/src/main/java/no/nav/foreldrepenger/fordel/web/app/rest/TimingFilter.java
@@ -13,17 +13,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.micrometer.core.instrument.Metrics;
+import no.nav.vedtak.log.metrics.MetricsUtil;
 
 @Provider
 public class TimingFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
+    private static final String METRIC_NAME = "rest";
     private static final Logger LOG = LoggerFactory.getLogger(TimingFilter.class);
     private static final ThreadLocalTimer TIMER = new ThreadLocalTimer();
+
+    public TimingFilter() {
+        MetricsUtil.utvidMedHistogram(METRIC_NAME);
+    }
 
     @Override
     public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
         var stop = TIMER.stop();
-        Metrics.timer(req.getUriInfo().getPath()).record(Duration.ofMillis(stop));
+        Metrics.timer(METRIC_NAME, "path", req.getUriInfo().getPath()).record(Duration.ofMillis(stop));
         LOG.info("Eksekvering {} tok {}ms", req.getUriInfo().getPath(), stop);
     }
 
