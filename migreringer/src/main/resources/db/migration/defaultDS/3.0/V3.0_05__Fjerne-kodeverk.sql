b4cf7e1266eecd89975814f91a8764564a186e33
Michal J. Sladek <michal.sladek@nav.no>
2022-05-10 11:11:30 +0200

    Legger ut ny baseline.

--- migreringer/src/main/resources/db/migration/defaultDS/3.0/V3.0_05__Fjerne-kodeverk.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE DOKUMENT SET UNUSED(KL_DOKUMENT_TYPE_ID);
-ALTER TABLE DOKUMENT SET UNUSED(KL_ARKIV_FILTYPE);
-
-drop table kodeliste cascade constraints;
-drop table kodeverk cascade constraints;
--- migreringer/src/main/resources/db/migration/defaultDS/3.0/V3.0_10__prosesstask-slett-type.sql
+++
@@ -1,2 +0,0 @@
-drop table PROSESS_TASK_TYPE cascade constraints ;
-drop table PROSESS_TASK_FEILHAND cascade constraints ;
