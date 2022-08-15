dc284e33ebd345f818398035bb029c4743b2227e
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-08-15 14:57:49 +0200

    Bruker gcp-variant i SkjermetPersonKlient (TFP-5094)

--- domene/src/main/java/no/nav/foreldrepenger/mottak/behandlendeenhet/nom/SkjermetPersonKlient.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/behandlendeenhet/nom/SkjermetPersonKlient.java
@@ -5,30 +5,34 @@ import java.net.URI;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import no.nav.vedtak.felles.integrasjon.rest.AzureADRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.foreldrepenger.konfig.KonfigVerdi;
-import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 
 /*
  * Klient for å sjekke om person er skjermet.
- * Grensesnitt https://skjermede-personer-pip.<intern-standard-dev-uri>/swagger-ui/index.html#/
+ * Grensesnitt https://skjermede-personer-pip.<intern-standard-dev-uri>/swagger-ui
  */
 @ApplicationScoped
 public class SkjermetPersonKlient {
 
-    private static final String DEFAULT_URI = "http://skjermede-personer-pip.nom/skjermet";
+    private static final String DEFAULT_URI = "https://skjermede-personer-pip.intern.nav.no";
+    private static final String DEFAULT_AZURE_SCOPE = "api://prod-gcp.nom.skjermede-personer-pip/.default";
 
     private static final Logger LOG = LoggerFactory.getLogger(SkjermetPersonKlient.class);
 
-    private OidcRestClient restClient;
     private URI uri;
+    
+    private AzureADRestClient client;
 
+    
     @Inject
-    public SkjermetPersonKlient(OidcRestClient restClient, @KonfigVerdi(value = "skjermet.person.rs.url", defaultVerdi = DEFAULT_URI) URI uri) {
-        this.restClient = restClient;
+    public SkjermetPersonKlient(@KonfigVerdi(value = "skjermet.person.rs.url", defaultVerdi = DEFAULT_URI) URI uri,
+                                @KonfigVerdi(value = "skjermet.person.rs.azure.scope", defaultVerdi = DEFAULT_AZURE_SCOPE) String scope) {
         this.uri = uri;
+        this.client = AzureADRestClient.builder().scope(scope).build();
     }
 
     public SkjermetPersonKlient() {
@@ -39,7 +43,7 @@ public class SkjermetPersonKlient {
     public boolean erSkjermet(String fnr) {
         if (fnr == null) return false;
         var request = new SkjermetRequestDto(fnr);
-        var skjermet = restClient.post(uri, request);
+        var skjermet = client.post(uri, request);
         if ("true".equalsIgnoreCase(skjermet)) {
             LOG.info("FPFORDEL skjermet person funnet");
             return true;
@@ -47,6 +51,6 @@ public class SkjermetPersonKlient {
         return false;
     }
 
-    private static record SkjermetRequestDto(String personident) {}
+    private record SkjermetRequestDto(String personident) {}
 
 }
