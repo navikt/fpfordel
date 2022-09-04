ecede985adac0be9d09b1f504fbf8deaba782142
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2022-09-04 20:32:54 +0200

    Ta i bruk nytt abac-grensesnitt

--- web/src/main/java/no/nav/foreldrepenger/fordel/web/server/abac/BeskyttetRessursAttributt.java
+++ web/src/main/java/no/nav/foreldrepenger/fordel/web/server/abac/BeskyttetRessursAttributt.java
@@ -2,5 +2,4 @@ package no.nav.foreldrepenger.fordel.web.server.abac;
 
 public class BeskyttetRessursAttributt {
     public static final String FAGSAK = "no.nav.abac.attributter.foreldrepenger.fagsak";
-    public static final String DRIFT = "no.nav.abac.attributter.foreldrepenger.drift";
 }
