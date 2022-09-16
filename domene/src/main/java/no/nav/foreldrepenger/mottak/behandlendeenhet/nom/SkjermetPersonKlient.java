399404e47730b9bd9b4e01b829ac59feab51329c
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-09-16 10:16:54 +0200

    Flytt trafikk til gcp-variant av skjermingsløsningen (#2022)

--- domene/src/main/java/no/nav/foreldrepenger/mottak/behandlendeenhet/nom/SkjermetPersonKlient.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/behandlendeenhet/nom/SkjermetPersonKlient.java
@@ -6,7 +6,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
 import no.nav.vedtak.felles.integrasjon.rest.AzureADRestClient;
-import no.nav.vedtak.felles.integrasjon.rest.OidcRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,35 +13,26 @@ import no.nav.foreldrepenger.konfig.KonfigVerdi;
 
 /*
  * Klient for å sjekke om person er skjermet.
- * Grensesnitt https://skjermede-personer-pip.<intern-standard-dev-uri>/swagger-ui
+ * Grensesnitt se #skjermingsløsningen
  */
 @ApplicationScoped
 public class SkjermetPersonKlient {
-
-    private static final String DEFAULT_URI_ONPREM = "http://skjermede-personer-pip.nom/skjermet";
 
     private static final String DEFAULT_URI = "https://skjermede-personer-pip.intern.nav.no/skjermet";
     private static final String DEFAULT_AZURE_SCOPE = "api://prod-gcp.nom.skjermede-personer-pip/.default";
 
     private static final Logger LOG = LoggerFactory.getLogger(SkjermetPersonKlient.class);
 
-    private URI uriGcp;
-    private URI uriOnprem;
+    private URI uri;
 
-    private AzureADRestClient clientGcp;
-
-    private OidcRestClient clientOnprem;
+    private AzureADRestClient client;
 
     
     @Inject
-    public SkjermetPersonKlient(@KonfigVerdi(value = "skjermet.person.rs.url.onprem", defaultVerdi = DEFAULT_URI_ONPREM) URI uriOnprem,
-                                @KonfigVerdi(value = "skjermet.person.rs.url", defaultVerdi = DEFAULT_URI) URI uriGcp,
-                                @KonfigVerdi(value = "skjermet.person.rs.azure.scope", defaultVerdi = DEFAULT_AZURE_SCOPE) String scope,
-                                OidcRestClient opremClient) {
-        this.uriGcp = uriGcp;
-        this.uriOnprem = uriOnprem;
-        this.clientOnprem = opremClient;
-        this.clientGcp = AzureADRestClient.builder().scope(scope).build();
+    public SkjermetPersonKlient(@KonfigVerdi(value = "skjermet.person.rs.url", defaultVerdi = DEFAULT_URI) URI uri,
+                                @KonfigVerdi(value = "skjermet.person.rs.azure.scope", defaultVerdi = DEFAULT_AZURE_SCOPE) String scope) {
+        this.uri = uri;
+        this.client = AzureADRestClient.builder().scope(scope).build();
     }
 
     public SkjermetPersonKlient() {
@@ -53,26 +43,12 @@ public class SkjermetPersonKlient {
     public boolean erSkjermet(String fnr) {
         if (fnr == null) return false;
         var request = new SkjermetRequestDto(fnr);
-        var skjermet = clientOnprem.post(uriOnprem, request);
-        sjekkGcp(skjermet, request);
-        if ("true".equalsIgnoreCase(skjermet)) {
+        var respons = client.post(uri, request);
+        if ("true".equalsIgnoreCase(respons)) {
             LOG.info("FPFORDEL skjermet person funnet");
             return true;
         }
         return false;
-    }
-
-    private void sjekkGcp(String onpremRespons, SkjermetRequestDto request) {
-        try {
-            var gcpRespons = clientGcp.post(uriGcp, request);
-            if (onpremRespons.equalsIgnoreCase(gcpRespons)) {
-                LOG.info("SkjermetPersonKlient gir likt resultat i gcp og onPrem");
-            } else {
-                LOG.info("SkjermetPersonKlient gir avvikende resultat i gcp {} og onPrem {}", gcpRespons, onpremRespons);
-            }
-        } catch (Exception e) {
-            LOG.info("SkjermetPersonKlient gir exception mot gcp", e);
-        }
     }
 
     private record SkjermetRequestDto(String personident) {}
