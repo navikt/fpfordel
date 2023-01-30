00cbbf0d683a790b9d83e878a67019e1e02415f6
anjaaalerud <anja.gotesen.aalerud@nav.no>
2023-01-30 14:08:16 +0100

    TFP-4124 lagt inn enums for fagsak status og ytelsetype

--- domene/src/main/java/no/nav/foreldrepenger/mottak/klient/FagsakJournalFøringDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/klient/FagsakJournalFøringDto.java
@@ -4,5 +4,8 @@ import no.nav.foreldrepenger.kontrakter.fordel.SaksnummerDto;
 
 import java.time.LocalDate;
 
-public record FagsakJournalFøringDto(SaksnummerDto saksnummer, String ytelseNavn, LocalDate opprettetDato, LocalDate endretDato, String status) {
+public record FagsakJournalFøringDto(SaksnummerDto saksnummer, FagsakYtelseType ytelseType, LocalDate opprettetDato, LocalDate endretDato, FagsakStatus status) {
+    enum FagsakStatus { OPPRETTET, UNDER_BEHANDLING, LOEPENDE, AVSLUTTET }
+    enum FagsakYtelseType { ENGANGSTØNAD, FORELDREPENGER, SVANGERSKAPSPENGER, UDEFINERT }
 }
+
