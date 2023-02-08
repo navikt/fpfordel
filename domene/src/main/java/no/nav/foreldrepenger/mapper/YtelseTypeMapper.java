93a58a8b4f21f02439117c3061eb5b5f48217bf1
Michal J. Sladek <michal.sladek@nav.no>
2023-02-08 18:45:36 +0100

    Bruk opprettSak v2 med ytelseType

---
+++ domene/src/main/java/no/nav/foreldrepenger/mapper/YtelseTypeMapper.java
@@ -0,0 +1,29 @@
+package no.nav.foreldrepenger.mapper;
+
+import no.nav.foreldrepenger.fordel.kodeverdi.YtelseType;
+import no.nav.foreldrepenger.mottak.klient.YtelseTypeDto;
+
+public class YtelseTypeMapper {
+
+    public static YtelseTypeDto mapTilDto(YtelseType ytelseType) {
+        if (null == ytelseType) {
+            return null;
+        }
+        return switch (ytelseType) {
+            case FORELDREPENGER -> YtelseTypeDto.FORELDREPENGER;
+            case SVANGERSKAPSPENGER -> YtelseTypeDto.SVANGERSKAPSPENGER;
+            case ENGANGSTØNAD -> YtelseTypeDto.ENGANGSTØNAD;
+        };
+    }
+
+    public static YtelseType mapFraDto(YtelseTypeDto ytelseTypeDto) {
+        if (null == ytelseTypeDto) {
+            return null;
+        }
+        return switch (ytelseTypeDto) {
+            case FORELDREPENGER -> YtelseType.FORELDREPENGER;
+            case SVANGERSKAPSPENGER -> YtelseType.SVANGERSKAPSPENGER;
+            case ENGANGSTØNAD -> YtelseType.ENGANGSTØNAD;
+        };
+    }
+}
---
+++ domene/src/test/java/no/nav/foreldrepenger/mapper/YtelseTypeMapperTest.java
@@ -0,0 +1,25 @@
+package no.nav.foreldrepenger.mapper;
+
+import no.nav.foreldrepenger.fordel.kodeverdi.YtelseType;
+import no.nav.foreldrepenger.mottak.klient.YtelseTypeDto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class YtelseTypeMapperTest {
+
+    @Test
+    void map_fra_dto() {
+        assertThat(YtelseTypeMapper.mapFraDto(YtelseTypeDto.ENGANGSTØNAD)).isEqualTo(YtelseType.ENGANGSTØNAD);
+        assertThat(YtelseTypeMapper.mapFraDto(YtelseTypeDto.FORELDREPENGER)).isEqualTo(YtelseType.FORELDREPENGER);
+        assertThat(YtelseTypeMapper.mapFraDto(YtelseTypeDto.SVANGERSKAPSPENGER)).isEqualTo(YtelseType.SVANGERSKAPSPENGER);
+    }
+
+    @Test
+    void map_til_dto() {
+        assertThat(YtelseTypeMapper.mapTilDto(YtelseType.ENGANGSTØNAD)).isEqualTo(YtelseTypeDto.ENGANGSTØNAD);
+        assertThat(YtelseTypeMapper.mapTilDto(YtelseType.FORELDREPENGER)).isEqualTo(YtelseTypeDto.FORELDREPENGER);
+        assertThat(YtelseTypeMapper.mapTilDto(YtelseType.SVANGERSKAPSPENGER)).isEqualTo(YtelseTypeDto.SVANGERSKAPSPENGER);
+    }
+
+}
