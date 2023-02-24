009b711fa25f7aa578d818016e7c1f4cb040b009
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2023-02-24 13:28:53 +0100

    Felles 4228

--- web/src/main/java/no/nav/foreldrepenger/fordel/web/server/jetty/SubjectHandlerCleanupRequestListener.java
+++ web/src/main/java/no/nav/foreldrepenger/fordel/web/server/jetty/SubjectHandlerCleanupRequestListener.java
@@ -7,9 +7,7 @@ import javax.servlet.annotation.WebListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import no.nav.vedtak.sikkerhet.context.SubjectHandler;
-import no.nav.vedtak.sikkerhet.context.ThreadLocalSubjectHandler;
-import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
+import no.nav.vedtak.sikkerhet.context.ContextCleaner;
 
 @WebListener
 public class SubjectHandlerCleanupRequestListener implements ServletRequestListener {
@@ -18,20 +16,11 @@ public class SubjectHandlerCleanupRequestListener implements ServletRequestListe
 
     @Override
     public void requestDestroyed(ServletRequestEvent sre) {
+
         try {
-            if (KontekstHolder.harKontekst()) {
-                KontekstHolder.fjernKontekst();
-                LOG.trace("KONTEKST fjernet fra KontekstHolder OK");
-            }
-            var subjectHandler = SubjectHandler.getSubjectHandler();
-            var subject = subjectHandler.getSubject();
-            if (subject != null) {
-                LOG.trace("Servlet request destroyed");
-                ThreadLocalSubjectHandler.class.cast(subjectHandler).setSubject(null);
-                LOG.trace("Subject fjernet fra ThreadLocal OK");
-            }
+            ContextCleaner.enusureCleanContext();
         } catch (Exception e) {
-            LOG.trace("Kunne ikke fjerne subject", e);
+            LOG.info("Kunne ikke fjerne subject", e);
         }
     }
 }
