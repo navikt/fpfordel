e23b097acf00897bee491865ec14f28c5029ad9e
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2023-05-10 08:59:19 +0200

    Teamdokumenthandtering url oppdatering
    Endret fra default til teamdokumenthandtering namespace for service discovery url

--- domene/src/main/java/no/nav/foreldrepenger/mottak/journal/dokarkiv/DokArkivKlient.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/journal/dokarkiv/DokArkivKlient.java
@@ -7,7 +7,7 @@ import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
 import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
 
 @Dependent
-@RestClientConfig(tokenConfig = TokenFlow.STS_CC, endpointProperty = "dokarkiv.base.url", endpointDefault = "http://dokarkiv.default/rest/journalpostapi/v1/journalpost")
+@RestClientConfig(tokenConfig = TokenFlow.STS_CC, endpointProperty = "dokarkiv.base.url", endpointDefault = "http://dokarkiv.teamdokumenthandtering/rest/journalpostapi/v1/journalpost")
 public class DokArkivKlient extends AbstractDokArkivKlient {
 
     protected DokArkivKlient() {
