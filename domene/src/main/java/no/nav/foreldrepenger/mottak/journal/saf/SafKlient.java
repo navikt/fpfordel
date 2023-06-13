04a1404ccf7aed457b0527d0871855485886c970
Michal J. Sladek <michal.sladek@nav.no>
2023-06-13 07:55:52 +0200

    DokArkiv klienten bruker Azure.

--- domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/SafKlient.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/journal/saf/SafKlient.java
@@ -7,7 +7,7 @@ import no.nav.vedtak.felles.integrasjon.saf.AbstractSafKlient;
 import javax.enterprise.context.Dependent;
 
 @Dependent
-@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "saf.base.url", endpointDefault = "https://saf.nais.adeo.no", scopesProperty = "saf.scopes", scopesDefault = "api://prod-fss.teamdokumenthandtering.saf/.default")
+@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "saf.base.url", endpointDefault = "http://saf.teamdokumenthandtering", scopesProperty = "saf.scopes", scopesDefault = "api://prod-fss.teamdokumenthandtering.saf/.default")
 public class SafKlient extends AbstractSafKlient {
     public SafKlient() {
         super();
