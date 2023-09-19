3194cb1b8ecab507e0654cfd55efc28c1d364b66
Michal J. Sladek <michal.sladek@nav.no>
2023-09-19 08:48:47 +0200

    Legger inn status og reservasjon.

--- migreringer/src/main/resources/db/migration/defaultDS/3.1/V3.1_01__TFP-5571-lokalt_oppgave_tabelle.sql
+++ migreringer/src/main/resources/db/migration/defaultDS/3.1/V3.1_01__TFP-5571-lokalt_oppgave_tabelle.sql
@@ -3,14 +3,16 @@ create table OPPGAVE
     ID NUMBER(19) not null
         constraint PK_OPPGAVE
             primary key,
-    OPPGAVE_TYPE VARCHAR2(20 char) not null,
-    JOURNALPOST_ID VARCHAR2(32 char) not null,
+    TYPE VARCHAR2(20 char) not null,
+    STATUS VARCHAR2(20 char) not null,
+    JOURNALPOST_ID VARCHAR2(32 char),
     BRUKER_ID VARCHAR2(19 char),
     BEHANDLING_TEMA VARCHAR2(20 char),
     PRIORITET VARCHAR2(20 char),
     BESKRIVELSE varchar2(200 char),
     ENHET VARCHAR2(10 char),
     FRIST TIMESTAMP(3) not null,
+    RESERVERT_AV VARCHAR2(20 char),
     VERSJON INTEGER default 0 not null,
     OPPRETTET_AV VARCHAR2(20 char) default 'VL' not null,
     OPPRETTET_TID TIMESTAMP(3) default systimestamp not null,
@@ -21,7 +23,9 @@ create table OPPGAVE
 
 comment on table OPPGAVE is 'Inneholder oppgaver som skal løses av saksbehandlere i journalføring.'
 /
-comment on column OPPGAVE.OPPGAVE_TYPE is 'Type av oppgaven f.eks JFR for journalføring. Mappes til Oppgavetype kodeverk.'
+comment on column OPPGAVE.TYPE is 'Type av oppgaven f.eks JFR for journalføring. Mappes til Oppgavetype kodeverk.'
+/
+comment on column OPPGAVE.STATUS is 'Status av oppgaven. Foreløpig AAPENT, FERDIGSTILT.'
 /
 comment on column OPPGAVE.JOURNALPOST_ID is 'ID til journalposten i JOARK'
 /
@@ -37,5 +41,11 @@ comment on column OPPGAVE.ENHET is 'Tildelt enhet som skal løse oppgaven.'
 /
 comment on column OPPGAVE.FRIST is 'Frist dato for å løse oppgaven.'
 /
+comment on column OPPGAVE.RESERVERT_AV is 'Lagrer identen til SBH som reserverer oppgaven.'
+/
 comment on column OPPGAVE.VERSJON is 'Teknisk versjonering av endringer.'
+/
+
+create index IDX_OPPGAVE_TYPE
+    on OPPGAVE (TYPE)
 /
