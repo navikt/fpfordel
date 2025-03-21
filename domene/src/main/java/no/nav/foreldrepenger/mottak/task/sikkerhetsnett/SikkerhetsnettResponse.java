72a240f969129ea275ea8b7f8e99988ac0dec0f4
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-03-21 09:32:20 +0100

    TFP-6044 forvent óbjekt-respons

--- domene/src/main/java/no/nav/foreldrepenger/mottak/task/sikkerhetsnett/SikkerhetsnettResponse.java
+++
@@ -1,8 +0,0 @@
-package no.nav.foreldrepenger.mottak.task.sikkerhetsnett;
-
-public record SikkerhetsnettResponse(String journalpostId,
-                                     String mottaksKanal,
-                                     String behandlingstema,
-                                     String journalforendeEnhet) {
-
-}
