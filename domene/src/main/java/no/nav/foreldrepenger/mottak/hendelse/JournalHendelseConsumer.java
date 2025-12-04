654b5858a3b3d7c21c245770e44a446f31151845
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2025-12-04 09:55:26 +0100

    TFP-6438 (Fase 3): Slutter å konsumere journalføringhendelser. (#2669)
    * TFP-6438 (Fase 3): Slutter å konsumere journalføringhendelser.
    
    * Fjerner cronExpression på 3 av prosesstaskene

--- domene/src/main/java/no/nav/foreldrepenger/mottak/hendelse/JournalHendelseConsumer.java
+++ domene/src/main/java/no/nav/foreldrepenger/mottak/hendelse/JournalHendelseConsumer.java
@@ -1,53 +1,53 @@
-package no.nav.foreldrepenger.mottak.hendelse;
-
-import no.nav.vedtak.server.Controllable;
-import no.nav.vedtak.server.LiveAndReadinessAware;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord;
-import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
-
-/*
- * Dokumentasjon https://confluence.adeo.no/pages/viewpage.action?pageId=432217859
- */
-@ApplicationScoped
-public class JournalHendelseConsumer implements LiveAndReadinessAware, Controllable {
-
-    private static final Logger LOG = LoggerFactory.getLogger(JournalHendelseConsumer.class);
-
-    private KafkaConsumerManager<String, JournalfoeringHendelseRecord> kcm;
-
-    JournalHendelseConsumer() {
-    }
-
-    @Inject
-    public JournalHendelseConsumer(JournalføringHendelseHåndterer journalføringHendelseHåndterer) {
-        this.kcm = new KafkaConsumerManager<>(journalføringHendelseHåndterer);
-    }
-
-    @Override
-    public boolean isAlive() {
-        return kcm.allRunning();
-    }
-
-    @Override
-    public boolean isReady() {
-        return isAlive();
-    }
-
-    @Override
-    public void start() {
-        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
-        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
-    }
-
-    @Override
-    public void stop() {
-        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
-        kcm.stop();
-    }
-}
+//package no.nav.foreldrepenger.mottak.hendelse;
+//
+//import no.nav.vedtak.server.Controllable;
+//import no.nav.vedtak.server.LiveAndReadinessAware;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//import jakarta.enterprise.context.ApplicationScoped;
+//import jakarta.inject.Inject;
+//import no.nav.joarkjournalfoeringhendelser.JournalfoeringHendelseRecord;
+//import no.nav.vedtak.felles.integrasjon.kafka.KafkaConsumerManager;
+//
+///*
+// * Dokumentasjon https://confluence.adeo.no/pages/viewpage.action?pageId=432217859
+// */
+//@ApplicationScoped
+//public class JournalHendelseConsumer implements LiveAndReadinessAware, Controllable {
+//
+//    private static final Logger LOG = LoggerFactory.getLogger(JournalHendelseConsumer.class);
+//
+//    private KafkaConsumerManager<String, JournalfoeringHendelseRecord> kcm;
+//
+//    JournalHendelseConsumer() {
+//    }
+//
+//    @Inject
+//    public JournalHendelseConsumer(JournalføringHendelseHåndterer journalføringHendelseHåndterer) {
+//        this.kcm = new KafkaConsumerManager<>(journalføringHendelseHåndterer);
+//    }
+//
+//    @Override
+//    public boolean isAlive() {
+//        return kcm.allRunning();
+//    }
+//
+//    @Override
+//    public boolean isReady() {
+//        return isAlive();
+//    }
+//
+//    @Override
+//    public void start() {
+//        LOG.info("Starter konsumering av topics={}", kcm.topicNames());
+//        kcm.start((t, e) -> LOG.error("{} :: Caught exception in stream, exiting", t, e));
+//    }
+//
+//    @Override
+//    public void stop() {
+//        LOG.info("Starter shutdown av topics={} med 10 sekunder timeout", kcm.topicNames());
+//        kcm.stop();
+//    }
+//}
